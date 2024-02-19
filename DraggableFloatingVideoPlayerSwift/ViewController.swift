8e625379dea123eb9bc5363626b34dd429983858
Md Khaled Hasan Manna <khaled.hasan@nexdecade.com>
2024-02-19 10:58:47 +0600

    Initial Commit

---
+++ DraggableFloatingVideoPlayerSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DraggableFloatingVideoPlayerSwift
+//
+//  Created by Khaled-iOS on 19/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
